da93581c9f6b9b008af3efbd10acf45acff439dc
kibi-00 <kibi_00@yahoo.com.ar>
2022-03-01 11:38:40 +0100

    Removed if/else function from Weather
    This allows for a new search to be carried out after a result has already been displayed

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import axios from "axios";
 
 export default function Weather(props) {
-  const [info, setInfo] = useState(null);
   const [temp, setTemp] = useState(null);
   const [desc, setDesc] = useState(null);
   const [humidity, setHumidity] = useState(null);
@@ -11,7 +10,7 @@ export default function Weather(props) {
 
   function showTemp(response) {
     console.log(response.data);
-    setInfo(`${props.city}`);
+
     setTemp(response.data.main.temp);
     setDesc(response.data.weather[0].description);
     setHumidity(response.data.main.humidity);
@@ -21,22 +20,18 @@ export default function Weather(props) {
     );
   }
 
-  if (info) {
-    return (
-      <ul>
-        <li>Temperature : {Math.round(temp)}</li>
-        <li>Description: {desc}</li>
-        <li>Humidity: {humidity}% </li>
-        <li>Wind: {wind}km/h</li>
-        <li>
-          <img src={icon} alt={desc} />
-        </li>
-      </ul>
-    );
-  } else {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=1d9f5e22b7d5d2800eb8802fb2e8da88&units=metric`;
-    axios.get(url).then(showTemp);
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=1d9f5e22b7d5d2800eb8802fb2e8da88&units=metric`;
+  axios.get(url).then(showTemp);
 
-    return <p>loading information for {props.city}</p>;
-  }
+  return (
+    <ul>
+      <li>Temperature : {Math.round(temp)}</li>
+      <li>Description: {desc}</li>
+      <li>Humidity: {humidity}% </li>
+      <li>Wind: {wind}km/h</li>
+      <li>
+        <img src={icon} alt={desc} />
+      </li>
+    </ul>
+  );
 }
